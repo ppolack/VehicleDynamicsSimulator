0f041bc70b2d45b40453cddf4035af5d1eaf2fd8
Philip Polack <philip.polack@stanley-robotics.com>
2020-04-29 18:15:50 +0200

    feat: update main

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,38 @@ int main() {
     VehicleSimulator vehicle_simulator = VehicleSimulator();
 
     std::cout << "Running main" << std::endl;
-    std::cout << vehicle_simulator.getState() << std::endl;
+
+    State  initial_state;
+    double vx_start = 0.2;
+    double wh_start = vx_start / vehicle_simulator.getVehicleModel().r_eff;
+    initial_state << 0.0, vx_start, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, wh_start, wh_start, wh_start,
+        wh_start;
+
+    Control control;
+    control << 100.0, 100.0, 0, 0, 0.02;
+
+    vehicle_simulator.setState(initial_state);
+
+    std::cout << "Start simulation" << std::endl;
+
+    for (int tt = 0; tt <= time_end; tt = tt + time_step) {
+        // define the control value
+        vehicle_simulator.setControl(control);
+
+        // compute slips and forces
+        Slips slips = vehicle_simulator.computeSlips();
+        vehicle_simulator.setSlips(slips);
+        Forces forces = vehicle_simulator.computeForces();
+        vehicle_simulator.setForces(forces);
+
+        // get new state
+        // State state = integration...;
+        // vehicle_simulator.setState(state);
+
+        std::cout << "X position: " << vehicle_simulator.getState()[0] << std::endl;
+        std::cout << "Y position: " << vehicle_simulator.getState()[2] << std::endl;
+        std::cout << "yaw position: " << vehicle_simulator.getState()[6] << std::endl;
+    }
 
     return 0;
 }
